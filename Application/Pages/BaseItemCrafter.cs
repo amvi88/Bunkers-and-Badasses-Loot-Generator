09d3d5dc2240bfaa0724331a19e25107f8bae0d4
Alberto M <malavassiv@gmail.com>
2023-03-18 21:34:38 -0600

    Refactor all Crafters to use common code and state

--- Application/Pages/BaseItemCrafter.cs
+++ Application/Pages/BaseItemCrafter.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Models;
+using Models.Builder;
 
 namespace Application.Pages
 {
-    public class BaseItemCrafter : BaseItemViewer 
+    public class BaseItemCrafter : BaseItemViewer
     {
+        public bool UseCustomImage { get; set; } = false;
+        public bool AccordionOneCollapsed { get; set; } = false;
+        public bool AccordionTwoCollapsed { get; set; } = true;
+        public bool AccordionThreeCollapsed { get; set; } = true;
+        public bool AccordionFourCollapsed { get; set; } = true;
+        public bool AccordionFiveCollapsed { get; set; } = true;
+        public bool UseBorderlandsManufacturers { get; set; } = false;
+
+        public List<Guild> guilds = null;
+
         public async Task<string> EncodeImageFile(InputFileChangeEventArgs eventArgs)
         {
             var files = eventArgs.GetMultipleFiles(1);
@@ -28,5 +39,41 @@ namespace Application.Pages
 
             return null;
         }
+
+        public void ToggleAccordionOne()
+        {
+            AccordionOneCollapsed = !AccordionOneCollapsed;
+            this.StateHasChanged();
+        }
+
+        public void ToggleAccordionTwo()
+        {
+            AccordionTwoCollapsed = !AccordionTwoCollapsed;
+            this.StateHasChanged();
+        }
+
+        public void ToggleAccordionThree()
+        {
+            AccordionThreeCollapsed = !AccordionThreeCollapsed;
+            this.StateHasChanged();
+        }
+
+        public void ToggleAccordionFour()
+        {
+            AccordionFourCollapsed = !AccordionFourCollapsed;
+            this.StateHasChanged();
+        }
+
+        public void ToggleAccordionFive()
+        {
+            AccordionFiveCollapsed = !AccordionFiveCollapsed;
+            this.StateHasChanged();
+        }
+
+        public string AccordionOneClass => !AccordionOneCollapsed ? "collapse show" : "collapse";
+        public string AccordionTwoClass => !AccordionTwoCollapsed ? "collapse show" : "collapse";
+        public string AccordionThreeClass => !AccordionThreeCollapsed ? "collapse show" : "collapse";
+        public string AccordionFourClass => !AccordionFourCollapsed ? "collapse show" : "collapse";
+        public string AccordionFiveClass => !AccordionFiveCollapsed ? "collapse show" : "collapse";
     }
 }
