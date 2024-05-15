c2ba7e03df757163a12d07bf7fb2fbcbb5995ebb
malavassiv <malavassiv@gmail.com>
2024-05-15 06:24:07 -0600

    Fix script do ignore cors options

--- Application/js/src/index.js
+++ Application/js/src/index.js
@@ -5,99 +5,97 @@ const isNotFirefox = navigator.userAgent.indexOf("Firefox") < 0;
 
 const snapshotCreator = (id) => {
 
-  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-    try {
-      const scale = window.devicePixelRatio;
-      const element = document.getElementById(id)
-      htmlToImage
-        .toBlob(element, {
-          height: element.offsetHeight * scale,
-          width: element.offsetWidth * scale,
-          style: {
-            transform: "scale(" + scale + ")",
-            transformOrigin: "top left",
-            width: element.offsetWidth + "px",
-            height: element.offsetHeight + "px",
-          },
-          useCorsEverywhereProxy: true
-        })
-        .then((blob) => {
-          resolve(blob);
-        });
+        try {
+            const scale = window.devicePixelRatio;
+            const element = document.getElementById(id)
+            htmlToImage
+                .toBlob(element, {
+                    height: element.offsetHeight * scale,
+                    width: element.offsetWidth * scale,
+                    style: {
+                        transform: "scale(" + scale + ")",
+                        transformOrigin: "top left",
+                        width: element.offsetWidth + "px",
+                        height: element.offsetHeight + "px",
+                    },
+                    useCorsEverywhereProxy: true,
+                    cacheBust: true,
+                    fetchRequestInit: { mode: 'no-cors' }
+                })
+                .then((blob) => {
+                    resolve(blob);
+                });
 
-    } catch (e) {
-      reject(e);
-    }
-  });
+        } catch (e) {
+            reject(e);
+        }
+    });
 };
 
-const copyImageToClipBoardSafari = (id) => 
-{
+const copyImageToClipBoardSafari = (id) => {
     navigator.clipboard
-      .write([
-        new ClipboardItem({
-          "image/png": new Promise(async (resolve, reject) => {
-            try {
-              const blob = await snapshotCreator(id);
-              resolve(new Blob([blob], { type: "image/png" }));
-            } catch (err) {
-              reject(err);
-            }
-          }),
-        }),
-      ])
-      .then(() => {
-      })
-      .catch((err) => {
-        console.error("Error:", err)
-      });
+        .write([
+            new ClipboardItem({
+                "image/png": new Promise(async (resolve, reject) => {
+                    try {
+                        const blob = await snapshotCreator(id);
+                        resolve(new Blob([blob], { type: "image/png" }));
+                    } catch (err) {
+                        reject(err);
+                    }
+                }),
+            }),
+        ])
+        .then(() => {
+        })
+        .catch((err) => {
+            console.error("Error:", err)
+        });
 }
 
 const copyImageToClipBoardOtherBrowsers = (id) => {
-   var permission = isNotFirefox? "clipboard-write" : "clipboardWrite";
+    var permission = isNotFirefox ? "clipboard-write" : "clipboardWrite";
     navigator?.permissions?.query({ name: permission })
-      .then(async (result) => {
-        if (result.state === "granted") {
-          const type = "image/png";
-          const blob = await snapshotCreator(id);
-          let data = [new ClipboardItem({ [type]: blob })];
-          navigator.clipboard
-            .write(data)
-            .then(() => {
-            })
-            .catch((err) => {
-              console.error("Error:", err)
-            });
-        }
-    });
-  }
-
-
-export function downloadCard(id, filename)
-{
-  var node = document.getElementById(id);
-  htmlToImage
-  .toPng(node)
-  .then(function (dataUrl) {
-    var link = document.createElement('a');
-        link.download = filename;
-        link.href = dataUrl;
-        link.click();
-  })
-  .catch(function (error) {
-    console.error("oops, something went wrong!", error);
-  });
+        .then(async (result) => {
+            if (result.state === "granted") {
+                const type = "image/png";
+                const blob = await snapshotCreator(id);
+                let data = [new ClipboardItem({ [type]: blob })];
+                navigator.clipboard
+                    .write(data)
+                    .then(() => {
+                    })
+                    .catch((err) => {
+                        console.error("Error:", err)
+                    });
+            }
+        });
 }
 
-export function copyCard(id){
-  if (isSafari)
-  {
-    copyImageToClipBoardSafari(id)
-  }
-  else
-  {
-    copyImageToClipBoardOtherBrowsers(id)
-  }  
+
+export function downloadCard(id, filename) {
+    var node = document.getElementById(id);
+    htmlToImage
+        .toPng(node)
+        .then(function (dataUrl) {
+            var link = document.createElement('a');
+            link.download = filename;
+            link.href = dataUrl;
+            link.click();
+        })
+        .catch(function (error) {
+            console.error("oops, something went wrong!", error);
+        });
+}
+
+export function copyCard(id) {
+    if (isSafari) {
+        copyImageToClipBoardSafari(id)
+    }
+    else {
+        copyImageToClipBoardOtherBrowsers(id)
+    }
 }
 
